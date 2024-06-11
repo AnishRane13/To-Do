2fe7e4e7221b5a9524579497f9939341d9117e83
AnishRane13 <anishrane178@gmail.com>
2024-06-11 18:03:11 +0530

    Made some changes in the folder structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import TaskForm from './TaskForm';
-import TaskItem from './TaskItem';
-import Filter from './Filter';
+import TaskForm from './components/TaskForm';
+import TaskItem from './components/TaskItem';
+import Filter from './components/Filter';
 
 function App() {
   const [tasks, setTasks] = useState([]);
